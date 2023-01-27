304fc45b05092839796a3356b7a772c8c0ae70a9
Rvega99 <112989004+Rvega99@users.noreply.github.com>
2023-01-27 08:38:26 -0600

    Creo que ya quedo

--- GPIO.c
+++ GPIO.c
@@ -2,30 +2,37 @@
 extern void Configurar_GPIO(void)
 {
     //Paso 1 Enceder el periferico Run Clock Gate Control GPIO
-    SYSCTL->RCGCGPIO |= (1<<1) | (1<<5) | (1<<8) | (1<<12);//enable GPIOF,GPION,GPIOJ
-    while((SYSCTL->PRGPIO&0x20)==0){};
+    SYSCTL->RCGCGPIO |= (1<<1) | (1<<5) | (1<<8) | (1<<12); //El registro RCGCGPIO proporciona al software la capacidad de habilitar y deshabilitar módulos GPIO en Modo de ejecución
+
+    while((SYSCTL->PRGPIO&0x20)==0){};  //The PRGPIO register indicates whether the GPIO modules are ready to be accessed by software following a change in status of power, Run mode clocking, or reset.
     //Paso 2
  //   GPIO_PORTB_AHB_LOCK_R = 0x4C4F434B;
  //   GPIO_PORTB_AHB_CR_R = (1<<3);
 
-    //Salidas
+
+    //Salidas //El registro GPIODIR es el registro de dirección de datos. 
     GPIOF_AHB->DIR |= (1<<0) | (1<<4);
     GPION->DIR |= (1<<0) | (1<<1);
     GPIOB_AHB->DIR |= (1<<3);
     //Entradas
     GPIOJ_AHB->DIR |= (0<<1) | (0<<0);
-    GPIOJ_AHB->PUR =  (1<<1) | (1<<0);
-    //Paso 3
+
+//El registro GPIOPUR es el registro de control pull-up. 
+    GPIOJ_AHB->PUR =  (1<<1) | (1<<0); 
+
+    //Paso 3   //El registro GPIOAFSEL es el registro de selección de control de modo
     GPIOJ_AHB->AFSEL |= 0x00;
     GPION->AFSEL |= 0x00;
     GPIOF_AHB->AFSEL |= 0x00;
     GPIOB_AHB->AFSEL |= 0x00;
 
+    //El registro (GPIOPCTL) se utiliza para seleccionar una de las funciones posibles. Cuadro 26-5 de la página 1806
     GPIOJ_AHB->PCTL|= 0x00;
     GPION->PCTL |= 0x00;
     GPIOF_AHB->PCTL |= 0x00;
     GPIOB_AHB->PCTL |= 0x00;
 
+    //El registro GPIODEN es el registro de habilitación digital.
     GPIOF_AHB->DEN |= (1<<0) | (1<<4);
     GPION->DEN |= (1<<0) | (1<<1);
     GPIOJ_AHB->DEN |= (1<<1) | (1<<0);
--- UART.c
+++ UART.c
@@ -2,15 +2,21 @@
 
 extern void Configurar_UART3(void)
 {
-    SYSCTL->RCGCUART  = (1<<3);   //Paso 1 (RCGCUART) pag.388 UART/modulo0 0->Disable 1->Enable
-    SYSCTL->RCGCGPIO |= (1<<0);     //Paso 2 (RCGCGPIO) pag.382 Enable clock port A
-    //(GPIOAFSEL) pag.1770 Enable alternate function
-    GPIOA_AHB->AFSEL = (1<<5) | (1<<4);
-    //GPIO Port Control (GPIOPCTL) PA4-> U3Rx PA5-> U3Tx pag.741
+    SYSCTL->RCGCUART  = (1<<3);             //El registro RCGCUART proporciona al software la capacidad de habilitar y deshabilitar los módulos UART en modo Ejecutar UART 3 0->Apagado 1->Encendido
+    
+    SYSCTL->RCGCGPIO |= (1<<0);             //El registro RCGCGPIO proporciona al software la capacidad de habilitar y deshabilitar módulos GPIO en modo de ejecución
+    
+    GPIOA_AHB->AFSEL = (1<<5) | (1<<4);     //(GPIOAFSEL) Habilitar funcion alterna 
+
+    //El registro GPIOPCTL se utiliza junto con el registro GPIOAFSEL y selecciona el registro específico
+    //señal periférica para cada pin GPIO cuando se utiliza el modo de función alternativa
     GPIOA_AHB->PCTL = (GPIOA_AHB->PCTL&0xFFFFFF00) | 0x00000011;// (1<<0) | (1<<4);//0x00000011
-    // GPIO Digital Enable (GPIODEN) pag.682
+
+    // El registro GPIODEN es el registro de habilitación digital
     GPIOA_AHB->DEN = (1<<0) | (1<<1);//PA4 PA5
+
     //UART0 UART Control (UARTCTL) pag.918 Apagar para configurar primero todo y luego encender hasta abajo 
+    //El registro UARTCTL es el registro de control. Todos los bits se borran al restablecer, excepto los bits Transmit Enable (TXE) y Receive Enable (RXE),
     UART3->CTL = (0<<9) | (0<<8) | (0<<0);
 
     // UART Integer Baud-Rate Divisor (UARTIBRD) pag.914
@@ -18,16 +24,23 @@ extern void Configurar_UART3(void)
         //  BRD = 80,000,000 / (16*19200) =260.4166
         //  UARTFBRD[DIVFRAC] = integer(.4166 * 64 + 0.5)= 27
 
+    //El registro UARTIBRD es la parte entera del valor divisor de velocidad en baudios. Todos los bits se borran al reiniciar.
     UART3->IBRD = 260;
-    // UART Fractional Baud-Rate Divisor (UARTFBRD) pag.915
-    UART3->FBRD = 27;
-    //  UART Line Control (UARTLCRH) pag.916
-    UART3->LCRH = (0x3<<5)|(1<<4);
-    //  UART Clock Configuration(UARTCC) pag.939
-    UART3->CC =(0<<0);
-    //Disable UART3 UART Control (UARTCTL) pag.918
-    UART3->CTL = (1<<0) | (1<<8) | (1<<9);
 
+    // Al cambiar el registro UARTFBRD, el nuevo valor no surte efecto hasta
+    //La transmisión/recepción del personaje actual se ha completado
+    UART3->FBRD = 27;
+
+    //  El registro UARTLCRH es el registro de control de línea. 
+    //Parámetros de serie como longitud de datos, paridad,  
+    //y la selección de bits de parada se implementan en este registro.
+    UART3->LCRH = (0x3<<5)|(1<<4);
+
+    //  El registro UARTCC controla la fuente del reloj en baudios para el módulo UART
+    UART3->CC =(0<<0);
+    
+    //Encender UART Control (UARTCTL) 
+    UART3->CTL = (1<<0) | (1<<8) | (1<<9);
 
 
 }
@@ -48,30 +61,59 @@ extern void printChar(char c)
     while((UART3->FR & (1<<5)) != 0 );
     UART3->DR = c;
 }
-extern void printString(char* string)
+extern void printString(char* string2)
 {
-    while(*string)
+    while(*string2)
     {
-        printChar(*(string++));
+        printChar(*(string2++));
     }
 }
 
-extern int readString(char delimitador,char *string)
+extern int readString(char delimitadorF,char *string,char *string2)
 {
-
+    int s=0;
+int j=0;
    int i=0;
    char c = readChar();
-   while(c != delimitador)
+   while(c != delimitadorF)
    {
-       *(string+i) = c;
+       string[i]=c;
        i++;
-       c = readChar();
+       c=readChar();
+   }
+   s=i*2;
+   int h=0;
+   for(j=0;j<s;j++)
+   {
+       
+       string2[h]=string[i-1];
+       h++;
+       string2[h]=j+1;
+       h++;
+       i--;
    }
 
    return i;
 
 }
+extern char invertirString(char *string)
+{
+    int i=0;
+int J=0;
+    int longitud = sizeof(string);
+  char temp;
+  for (int i= 0, j = longitud - 1; i < (longitud / 2); i++, J--) 
+  {
+    temp= string[i];
+    string[i] = string[J];
+    string[J] = temp;
+  }
+  return string;
+}
 //Experimento 2
+
 //El envio es su nombre  (rave) 
+
 // invertirlo y regresarlo con numeros consecutivos
 // entre letras (e1v2a3r) 
+
